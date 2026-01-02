d5e1dc7358b816d53ff6a89d98a179d49b342993
Md Sahil <jigyasayadav361@gmail.com>
2026-01-02 22:24:06 +0530

    T

--- src/js/chatPage.js
+++ src/js/chatPage.js
@@ -31,16 +31,27 @@ let selectedUserId;
 let currentUser;
 let userProfilePic;       // user you are chatting with
 let activeCallType = null; // "audio" | "video" | null
+// const rtcConfig = {
+//   iceServers: [
+//     { urls: "stun:stun.l.google.com:19302" },
+//     {
+//       urls: [
+//         "turn:global.relay.metered.ca:80",
+//         "turn:global.relay.metered.ca:443"
+//       ],
+//       username: "b78ae65feed0d617772df2d",
+//       credential: "toMiYI0WXzR7hGjj"
+//     }
+//   ]
+// };
+
 const rtcConfig = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
     {
-      urls: [
-        "turn:global.relay.metered.ca:80",
-        "turn:global.relay.metered.ca:443"
-      ],
-      username: "b78ae65feed0d617772df2d",
-      credential: "toMiYI0WXzR7hGjj"
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject"
     }
   ]
 };
@@ -725,7 +736,10 @@ answerBtn.onclick = async () => {
     videoPeer.addTrack(track, videoStream);
   });
 
-  videoPeer.ontrack = (e) => {
+  videoPeer.ontrack = (e) => {videoPeer.oniceconnectionstatechange = () => {
+  console.log("ICE:", videoPeer.iceConnectionState);
+};
+
 
     remoteVideo.srcObject = e.streams[0];
   };
